e86b4280bfb3247d00cc90671dfc82d6560e9caf
ppip <ppip@duck.com>
2026-09-01 17:15:54 +0800

    refactor: clarify pretransformed fastmod usage

--- misc/fastmod.cpp
+++ misc/fastmod.cpp
@@ -1,5 +1,5 @@
 // @title 快速模乘
-// @brief 要求 1<=P<2^31 且 a<P；复用 trans(z) 时第一个 mul 更快
+// @brief 要求 1<=P<2^31 且 a<P；同一 z 多次使用时先 trans，再用 mul_t
 // @complexity O(1)
 // @author ppip
 
@@ -12,9 +12,10 @@ inline u64 trans(u64 x) {
 	constexpr u64 q=((u128(-(u64)P%P)<<64)+P-1)/P;
 	return x*A+u64((u128)x*q>>64)+1;
 }
-inline void mul(u64& a,u64 z) {
-	a=a*z*(u128)P>>64;
+// t 必须是 trans(z) 的返回值；复用同一 z 时只需计算一次 t
+inline u32 mul_t(u32 a,u64 t) {
+	return a*t*(u128)P>>64;
 }
 inline u32 mul(u32 a,u64 z) {
-	return a*trans(z)*(u128)P>>64;
+	return mul_t(a,trans(z));
 }
